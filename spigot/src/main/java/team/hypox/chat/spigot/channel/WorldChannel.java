afd10675941f1a9d112cbc451299253b93d65faa
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2022-04-21 00:46:37 -0500

    use audience provider in world channel

---
+++ spigot/src/main/java/team/hypox/chat/spigot/channel/WorldChannel.java
@@ -0,0 +1,35 @@
+package team.hypox.chat.spigot.channel;
+
+import org.bukkit.World;
+import team.hypox.chat.core.channel.AbstractChannel;
+import team.hypox.chat.core.channel.ChannelCondition;
+import team.hypox.chat.core.channel.decorator.DecoratorChannelCondition;
+import team.hypox.chat.core.member.ChannelMember;
+import team.hypox.chat.core.message.MessageContext;
+import team.hypox.chat.core.util.ChannelMemberList;
+import team.hypox.chat.spigot.bukkit.AudienceProvider;
+import team.hypox.chat.spigot.condition.WorldChannelCondition;
+
+public class WorldChannel extends AbstractChannel {
+
+	private final AudienceProvider audienceProvider;
+	private final World world;
+
+	private final ChannelCondition condition;
+
+	public WorldChannel(AudienceProvider audienceProvider, World world) {
+		this.audienceProvider = audienceProvider;
+		this.world = world;
+		this.condition = new WorldChannelCondition(world, new DecoratorChannelCondition.EndDecorator());
+	}
+
+	@Override
+	public boolean canListen(ChannelMember member, MessageContext ctx) {
+		return condition.canListen(member, ctx);
+	}
+
+	@Override
+	protected ChannelMemberList audience() {
+		return audienceProvider.audience(world);
+	}
+}
