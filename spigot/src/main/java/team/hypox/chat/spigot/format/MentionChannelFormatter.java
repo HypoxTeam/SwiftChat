582062ffdd963b44908ea12ac1c33720a36f5005
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2022-04-21 00:48:03 -0500

    added mention formatter

---
+++ spigot/src/main/java/team/hypox/chat/spigot/format/MentionChannelFormatter.java
@@ -0,0 +1,51 @@
+package team.hypox.chat.spigot.format;
+
+import org.bukkit.ChatColor;
+import team.hypox.chat.core.channel.ChannelFormatter;
+import team.hypox.chat.core.channel.decorator.DecoratorChannelFormatter;
+import team.hypox.chat.core.member.ChannelMember;
+import team.hypox.chat.core.message.MessageContext;
+import team.hypox.chat.spigot.settings.mention.MentionSettings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MentionChannelFormatter extends DecoratorChannelFormatter {
+
+	private final static Pattern MENTION_REGEX = Pattern.compile("@.([a-zA-Z0-9_\\-.]+)");
+	private final MentionSettings mentionService;
+
+	public MentionChannelFormatter(ChannelFormatter formatter, MentionSettings mentionService) {
+		super(formatter);
+		this.mentionService = mentionService;
+	}
+
+	@Override
+	public MessageContext formatMessage(MessageContext ctx, ChannelMember recipient) {
+		return formatMention(super.formatMessage(ctx, recipient), recipient);
+	}
+
+	private MessageContext formatMention(MessageContext ctx, ChannelMember recipient) {
+		String formattedMessage = ctx.formattedMessage();
+
+		Matcher matcher = MENTION_REGEX.matcher(formattedMessage);
+		StringBuffer buffer = new StringBuffer();
+
+		while (matcher.find()) {
+			String userMentioned = matcher.group().substring(1);
+
+			if (userMentioned.equalsIgnoreCase(recipient.name())) {
+				mentionService.execute();
+
+				matcher.appendReplacement(
+						buffer,
+						mentionService.colorize() + "@" + userMentioned + ChatColor.getLastColors(ctx.formattedMessage())
+				);
+			}
+		}
+
+		matcher.appendTail(buffer);
+		ctx.formatMessage(buffer.toString());
+		return ctx;
+	}
+}
