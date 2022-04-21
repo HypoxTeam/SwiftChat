7ee52b5cc61f84c12b753030aaebfd8a8c5c1864
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2022-04-21 00:47:01 -0500

    added server channel

---
+++ spigot/src/main/java/team/hypox/chat/spigot/channel/ServerChannel.java
@@ -0,0 +1,19 @@
+package team.hypox.chat.spigot.channel;
+
+import team.hypox.chat.core.channel.AbstractChannel;
+import team.hypox.chat.core.util.ChannelMemberList;
+import team.hypox.chat.spigot.bukkit.AudienceProvider;
+
+public class ServerChannel extends AbstractChannel {
+
+	private final AudienceProvider audienceProvider;
+
+	public ServerChannel(AudienceProvider audienceProvider) {
+		this.audienceProvider = audienceProvider;
+	}
+
+	@Override
+	protected ChannelMemberList audience() {
+		return audienceProvider.audience();
+	}
+}
