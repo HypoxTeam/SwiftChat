86840d9cf2581a04ef663261b54b2f8e461e3a5c
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2022-04-21 00:48:19 -0500

    added radial channel condition

---
+++ spigot/src/main/java/team/hypox/chat/spigot/condition/RadialChannelCondition.java
@@ -0,0 +1,34 @@
+package team.hypox.chat.spigot.condition;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import team.hypox.chat.core.channel.ChannelCondition;
+import team.hypox.chat.core.channel.decorator.DecoratorChannelCondition;
+import team.hypox.chat.core.member.ChannelMember;
+import team.hypox.chat.core.message.MessageContext;
+
+public class RadialChannelCondition extends DecoratorChannelCondition {
+
+	private final int radio;
+
+	public RadialChannelCondition(ChannelCondition condition, int radio) {
+		super(condition);
+		this.radio = radio;
+	}
+
+	@Override
+	public boolean canListen(ChannelMember recipient, MessageContext ctx) {
+		return super.canListen(recipient, ctx) && isInRadius(recipient, ctx);
+	}
+
+	private boolean isInRadius(ChannelMember recipient, MessageContext ctx) {
+		Player recipientPlayer = Bukkit.getPlayer(recipient.id());
+		Player authorPlayer = Bukkit.getPlayer(ctx.author().id());
+
+		if (recipientPlayer == null || authorPlayer == null) {
+			return false;
+		}
+
+		return recipientPlayer.getLocation().distanceSquared(authorPlayer.getLocation()) <= radio;
+	}
+}
