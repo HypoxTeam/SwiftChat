347c7725551921fb7f793fb17839cf174a6de093
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2022-05-09 13:03:29 -0500

    feat: added audience namespace impl

---
+++ spigot/src/main/java/team/hypox/chat/spigot/bukkit/AudienceArgumentProcessorImpl.java
@@ -0,0 +1,27 @@
+package team.hypox.chat.spigot.bukkit;
+
+import org.bukkit.Bukkit;
+import team.hypox.chat.core.extend.AudienceArgumentProcessor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class AudienceArgumentProcessorImpl implements AudienceArgumentProcessor {
+
+	private final Map<String, Function<Object, Object>> processors = new HashMap<>();
+
+	public AudienceArgumentProcessorImpl() {
+		processors.put("world", o -> Bukkit.getWorld((String) o));
+	}
+
+	@Override
+	public Object processArgument(String type, Object arg) {
+		return processors.get(type).apply(arg);
+	}
+
+	@Override
+	public void addProcessor(String type, Function<Object, Object> processor) {
+		processors.put(type, processor);
+	}
+}
--- spigot/src/main/java/team/hypox/chat/spigot/bukkit/AudienceFactory.java
+++ spigot/src/main/java/team/hypox/chat/spigot/bukkit/AudienceFactory.java
@@ -3,19 +3,46 @@ package team.hypox.chat.spigot.bukkit;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import team.hypox.chat.core.extend.AudienceArgumentProcessor;
+import team.hypox.chat.core.extend.AudienceNamespace;
 import team.hypox.chat.core.structure.member.cache.MemberCache;
 import team.hypox.chat.core.util.AudienceProvider;
 import team.hypox.chat.core.util.ChannelMemberList;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
-public class AudienceFactory {
+public class AudienceFactory implements AudienceNamespace {
 
 	private final MemberCache<Player> memberCache;
+	private final AudienceArgumentProcessor argumentProcessor;
 
-	public AudienceFactory(MemberCache<Player> memberCache) {
+	private final Map<String, Function<Object, AudienceProvider>> namespaces = new HashMap<>();
+
+	public AudienceFactory(MemberCache<Player> memberCache, AudienceArgumentProcessor argumentProcessor) {
 		this.memberCache = memberCache;
+		this.argumentProcessor = argumentProcessor;
+
+		namespaces.put("server", (unused) -> server());
+		namespaces.put("world", (world) -> world((World) world));
+	}
+
+	@Override
+	public AudienceArgumentProcessor argumentProcessor() {
+		return argumentProcessor;
+	}
+
+	@Override
+	public <T> AudienceProvider using(String namespace, T consumer) {
+		return namespaces.get(namespace).apply(consumer);
+	}
+
+	@Override
+	public void use(String namespace, Function<Object, AudienceProvider> factory) {
+		namespaces.put(namespace, factory);
 	}
 
 	public AudienceProvider server() {
