e68504b6cf5ee236b1523e9a3b99a623f6f4e416
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2022-05-09 13:02:50 -0500

    feat: added createFile in factory

--- core/src/main/java/team/hypox/chat/core/configuration/ConfigurationFactory.java
+++ core/src/main/java/team/hypox/chat/core/configuration/ConfigurationFactory.java
@@ -1,7 +1,11 @@
 package team.hypox.chat.core.configuration;
 
+import java.io.File;
+
 public interface ConfigurationFactory {
 
 	Configuration create(String name);
+
+	File createFile(String name);
 
 }
